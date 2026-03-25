58d686f2e27d32397faff62dbd2d5d4809e9fc8b
vi0650 <vaibhavvpadmani650@gmail.com>
2026-03-25 15:37:42 +0530

    connection fix

--- Frontend/src/environment/environment.prod.ts
+++ Frontend/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://invoiceapp-qqx4.onrender.com/api'
-}
+  production: true,
+  apiUrl: 'https://invoiceapp-qqx4.onrender.com/api'
+};
--- Frontend/src/environment/environment.ts
+++ Frontend/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7131/api'
-}
+  production: true,
+  apiUrl: 'https://invoiceapp-qqx4.onrender.com/api'
+};
